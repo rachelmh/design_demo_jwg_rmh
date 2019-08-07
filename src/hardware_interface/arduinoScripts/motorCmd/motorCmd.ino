714f87f8da68d0831cc1714a7a88ef500ce3ed60
Jacob Guggenheim <jguggenh@mit.edu>
2019-08-07 14:15:18 -0400

    asdf

--- src/hardware_interface/arduinoScripts/motorCmd/motorCmd.ino
+++ src/hardware_interface/arduinoScripts/motorCmd/motorCmd.ino
@@ -1,4 +1,6 @@
 #include "motorClass.h"
+#include <ArduinoHardware.h>
+#include "DualMC33926MotorShield.h"
 
 #include <ros.h>
 #include <std_msgs/Float32.h>
@@ -6,13 +8,18 @@
 // Robotzone motors
 float z_actuator_gear_ratio = 721;
 float z_actuator_enc_cnts_per_rev = 48.0;
-float yaw_actuator_gear_ratio = 721;
+float yaw_actuator_gear_ratio = 51;
 float yaw_actuator_enc_cnts_per_rev = 48.0;
 
 // create motor objects
 //motorClass::motorClass(int pwmPin,int dirPin, int encPin,float gearRatio, float encCntsRev)
-motorClass z_actuator =  motorClass(3,4,8,z_actuator_gear_ratio,z_actuator_enc_cnts_per_rev); 
-motorClass yaw_actuator =  motorClass(5,6,9,yaw_actuator_gear_ratio,yaw_actuator_enc_cnts_per_rev);
+motorClass z_actuator =  motorClass(5,3,7,z_actuator_gear_ratio,z_actuator_enc_cnts_per_rev); 
+motorClass yaw_actuator =  motorClass(6,4,8,yaw_actuator_gear_ratio,yaw_actuator_enc_cnts_per_rev);
+
+// gripper motor stuff
+DualMC33926MotorShield md;
+int gripperSpeed = 100;
+
 
 // ros stuff
 ros::NodeHandle arduino2Motor;
@@ -23,9 +30,9 @@ ros::Publisher testingPub("testing", &testing);
 
 // home and open door position for actuators
 int z_actuator_home_pos = 0; //TODO
-int z_actuator_open_door_pos = 0; //TODO
+int z_actuator_open_door_pos = 20; //TODO
 int yaw_actuator_home_pos = 0; //TODO
-int yaw_actuator_open_door_pos = 0; //TODO
+int yaw_actuator_open_door_pos = .5; //TODO
 
 void z_actuator_callback(const std_msgs::Float32& command)
 {
@@ -57,13 +64,31 @@ void yaw_actuator_callback(const std_msgs::Float32& command)
 	}
 }
 
+void gripper_callback(const std_msgs::Float32& command)
+{
+  // go home
+  if (command.data == 0)
+  {
+    md.setM1Speed(-1*gripperSpeed);
+  }
+  
+  // go to open door position
+  if (command.data == 1)
+  {
+    md.setM1Speed(gripperSpeed);
+  }
+
+  // stop moving!
+  if (command.data == 2)
+  {
+    md.setM1Speed(0);
+  }
+}
+
 // robotzone subscribers
 ros::Subscriber<std_msgs::Float32> z_actuator_command_sub("z_actuator_command", &z_actuator_callback);
 ros::Subscriber<std_msgs::Float32> yaw_actuator_command_sub("yaw_actuator_command", &yaw_actuator_callback);
-
-// gripper subscriber
-//ros::Subscriber<std_msgs::Float32> measurementSub("measurement2Arduino", &measurementCallback );
-
+ros::Subscriber<std_msgs::Float32> gripper_command_sub("gripper_command", &gripper_callback);
 
 void setup () 
 { 
@@ -73,7 +98,7 @@ void setup ()
   // subscriber
   arduino2Motor.subscribe(z_actuator_command_sub);
   arduino2Motor.subscribe(yaw_actuator_command_sub);
-  arduino2Motor.subscribe(measurementSub);
+  arduino2Motor.subscribe(gripper_command_sub);
 
   // advertise testing publisher
   arduino2Motor.advertise(testingPub);
@@ -90,7 +115,7 @@ void loop ()
   if (printing > 1000)
   {  
 	  // send testing
-	  testing.data = forceMotor.currentCommandfd;
+	  testing.data = z_actuator.MotorPos;
 	  testingPub.publish(&testing);
 
 	  // Serial.println(forceMotor.MotorForce);
@@ -100,8 +125,8 @@ void loop ()
   }
   
   // control robotzone motors
-  z_actuator_command_sub.pos_closedLoopController();
-  yaw_actuator_command_sub.pos_closedLoopController();
+  z_actuator.pos_closedLoopController();
+  yaw_actuator.pos_closedLoopController();
 
   printing = printing + 1;
 
--- src/high_level_controller/python_scripts/high_level_controller.py
+++ src/high_level_controller/python_scripts/high_level_controller.py
@@ -1,30 +1,40 @@
 #!/usr/bin/env python
+
 import rospy
 import time
 import random
 from std_msgs.msg import Float32
 from sensor_msgs.msg import Joy
 
+gripper_delay_move_flag = 0 # whether we should be delaying the grippers move
+gripper_move_flag = 0 # whether we should be moving the gripper
+gripper_go_home_flag = 0 # which direction to move gripper
+gripper_go_open_door_flag = 0 # which direction to move gripper
+
 def joy_callback(data):
 	# check if pressing "A"
 	if data.buttons[0] == 1:
 		# basically a go signal for predetermined return to home position
-		z_actuator_command_pub.publish(0) 
-		
+		z_actuator_command_pub.publish(0)
+
 	# check if pressing "B"
 	if data.buttons[1] == 1:
 		# basically a go signal for predetermined action primitive
-		z_actuator_command_pub.publish(1) 
-		
+		z_actuator_command_pub.publish(1)
+
 	# check if pressing "X"
 	if data.buttons[2] == 1:
 		# basically a go signal for predetermined return to home position
-		yaw_actuator_command_pub.publish(0)
-		
+		# yaw_actuator_command_pub.publish(0)
+		gripper_move_flag = 1 # can start move with yaw axis
+		gripper_go_home_flag = 1
+
 	# check if pressing "Y"
 	if data.buttons[3] == 1:
 		# basically a go signal for predetermined action primitive
-		yaw_actuator_command_pub.publish(1)
+		# yaw_actuator_command_pub.publish(1)
+		gripper_delay_move_flag = 1 # have to delay move
+		gripper_go_open_door_flag = 1
 
 
 def main():
@@ -36,10 +46,12 @@ def main():
 	z_actuator_command_pub = rospy.Publisher('z_actuator_command', Float32, queue_size = 10)
 	global yaw_actuator_command_pub
 	yaw_actuator_command_pub = rospy.Publisher('yaw_actuator_command', Float32, queue_size = 10)
+	global gripper_command_pub
+	gripper_command_pub = rospy.Publisher('gripper_command', Float32, queue_size = 10)
 
 	# put both actuators to home positions
-	z_actuator_command_pub.publish(0) 
-	yaw_actuator_command_pub.publish(0) 
+	#z_actuator_command_pub.publish(0)
+	#yaw_actuator_command_pub.publish(0)
 	time.sleep(3)
 
 	# initialize joy subscriber
@@ -48,6 +60,54 @@ def main():
 
 	# rospy.spin()
 	rospy.spin()
+
+	# high level control loop
+	control_loop()
+
+def control_loop():
+	# send send send
+	readyCount = 0
+	pubRate = 50 # hertz
+	rate = rospy.Rate(pubRate) # hertz
+
+	# gripper move variables
+	gripper_delay_move_counter = 0
+	gripper_move_counter = 0
+	gripper_delay_move_time = 3 # how long to delay the gripper moving (seconds)
+	gripper_move_time = 3 # how long to move the gripper for (seconds)
+
+	while (not rospy.is_shutdown()):
+		# check if the gripper is being delayed
+		if gripper_delay_move_flag == 1:
+			gripper_delay_move_counter = gripper_delay_move_counter + 1
+
+		# check if the gripper should begin being moved
+		if gripper_delay_move_counter >= gripper_delay_move_time*pubRate:
+			gripper_move_flag = 1
+			gripper_delay_move_counter = 0
+			gripper_delay_move_flag = 0
+
+		# move the gripper
+		if gripper_move_flag == 1:
+			if gripper_go_home_flag == 1:
+				pass
+				#gripper_command_pub.publish(0)
+			if gripper_go_open_door_flag == 1:
+				pass
+				#gripper_command_pub.publish(1)
+			gripper_move_counter = gripper_move_counter + 1
+
+		# check if the gripper should still be being moved
+		if gripper_move_counter >= gripper_move_time*pubRate :
+			gripper_go_home_flag = 0
+			gripper_go_open_door_flag = 0
+			gripper_move_counter = 0
+			gripper_move_flag = 0
+			#gripper_command_pub.publish(2) # stop command
+
+		# sleep to maintain loop rate
+		rate.sleep()
+
 
 def parse_force_glove():
 	# eventually parse force glove and send action primitives go signals. for now...
